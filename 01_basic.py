6372a3891c8d7615076b1f8c61b851f68f63888b
目的明确的阴谋家 <1224048623@qq.com>
2021-03-08 19:11:36 +0800

    basic

--- 01_basic.py
+++ 01_basic.py
@@ -7,3 +7,33 @@ a=10
 b=20
 print(a,b,a,b,a)
 
+# print的比较有意思用法如下 end=''意思是传入的是空字符串，末尾替换换行符号
+import math
+print("hello nihao",end='')
+print("hello zaijian")
+
+# 输出黑色与白色方块字符
+print(chr(9632),end='')
+print(chr(9633))
+
+def print_rate(nums=20,rate=0):
+    black_num=math.floor(nums*rate)
+    white_num=nums-black_num
+    print('|',end='')
+    for i in range(black_num):
+        print(chr(9632),end='')
+    for i in range(white_num):
+        print(chr(9633),end='')
+    print(" {:>6.2f}% ".format(100*rate),end='')
+    print('|',end='')
+
+# 下面开始
+sum=0
+for i in range(10000):
+    for j in range(100000):
+        sum+=j
+        sum-=j
+        sum+=j
+        sum-=j
+    print('\r',end='')
+    print_rate(rate=i/10000)
